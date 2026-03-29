e373256b72b420618488a99d13576be7cd3cc8f0
sahilgupta630 <guptasahilkumar783@gmail.com>
2026-03-29 17:19:46 +0530

    Initial setup with Vite & React

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import AboutEducation from "./components/AboutEducation";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Achievements from "./components/Achievements";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="bg-slate-950 font-sans text-slate-200">
+      <Navbar />
+      <main>
+        <Hero />
+        <AboutEducation />
+        <Experience />
+        <Projects />
+        <Achievements />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
